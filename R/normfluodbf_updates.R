098bfeb5c29bf2070093187265a97c31b1c1e6eb
alphaprime7 <awesome.tingwei@outlook.com>
2023-10-08 14:16:14 -0500

    up-update coming soon

--- R/normfluodbf_updates.R
+++ R/normfluodbf_updates.R
@@ -64,7 +64,10 @@ col_conv
 nona_updated <- nona_dat
 nona_updated[ , col_conv] <- lapply(nona_updated[ , col_conv],  # Convert data
                                        function(x){ as.numeric(as.character(gsub(",", "", x))) })
+
 dat_cycles <- unique_identifier(nona_updated)
+
+#SUBSAMPLES NOT WORKING BECAUSE DATA IS JANKY
 sub_samples <- dat_cycles[c(1:40),]
 plot(sub_samples$unique_id, sub_samples$V1)
 plot(sub_samples$unique_id, sub_samples$V2)
@@ -77,3 +80,91 @@ lipo2_updated[ , col_conv] <- lapply(lipo2_updated[ , col_conv],  # Convert data
 lipo2_updated[, c(1:ncol(lipo2_updated))] <- sapply(lipo2_updated[, c(1:ncol(lipo2_updated))], as.numeric)
 is.numeric(lipo2_updated)
 lipo2_clean <- na.omit(lipo2_updated)
+
+
+## REARRANGE THE DATA INTO A USABLE FORMAT, NO COMPLICATED MOVES
+## file = dat_cycles IS THE RIGHT FILE
+
+dat_cycles <- as.data.frame(dat_cycles)
+col_1 <- dat_cycles[,1]
+col_1 <- as.data.frame(col_1)
+
+
+#CLOSE TO THE UPDATE-# of cycles,# of samples
+col_1c <- data.frame()
+for(i in col_1){
+  col_1c <- rbind(col_1c,i)
+}
+
+resample_dat_close <- function(df, tnp, cycles){
+
+  sample_size <- c(1:cycles)
+  type_size <- c(1:tnp) #tnp is the sample types (3 for all, 2 for tp-tn-np if fixing something)
+  k <- c((1-tnp):(tnp-tnp)) # 1:3, 4:7, 115:117, 118:120 kth element < 120 based on standard setup
+
+  resulting_df <- data.frame()
+  for (i in 1:(nrow(df)/3)){
+
+    increment = tnp #(tnp=3 for 3 samples; tnp =2 for 2 samples)
+    k <- k + increment
+
+    colnames(resulting_df) = NULL
+    insert_row = df[k,]
+    colnames(insert_row) = NULL
+
+    resulting_df[i,type_size] <- rbind(insert_row, resulting_df)
+
+
+  }
+  suppressWarnings(return(resulting_df))
+}
+
+trial1 <- resample_dat_close(col_1, tnp = 3, cycles = 40)
+
+
+#tnp=testnegpos, cols = vector c(123)-match tnp, n = number of samples per group(tnp), cycles=chosen samples (40),
+resample_redo <- function(df, cols, tnp, n, cycles){
+
+  resulting_df <- data.frame()
+
+  for (i in 1:(ncol(col_1c)/3)){
+    for (j in tnp * seq(cycles/tnp)){
+
+      colnames(resulting_df) = NULL
+      insert_row = col_1c[,cols]
+      print(insert_row)
+      colnames(insert_row) = NULL
+      resulting_df[i,cols] <- rbind(resulting_df, insert_row)
+      print(resulting_df)
+
+      increment = 3
+      i = i + increment
+      j = j + increment
+
+      }
+  }
+  return(resulting_df)
+}
+
+resample_redo(col_1c, c(1:3), 3, 12, 40)
+
+#BETTER TO GO FROM COLUMN TO ROWS INSTEAD
+#3 in the loop is the # of sample types (pos,neg vs test)
+#i can deduce your number of cycles from the setup and type of samples (advice that odd number samples are good for algos in this case)
+# 39 cycles is easier to code than 40 cycles at least so far, still working on it
+13
+46
+79
+1012
+1315
+1618
+1921
+2224
+2527
+2830
+3133
+3436
+3740
+.
+115117
+118120
